43fa0ee509c6f7a0b0cfe4e7d528a901dc7dbf3a
functionfaiecho <nurfaizahmkhalik@outlook.com>
2024-06-19 18:39:57 +1200

    Part 2.

---
+++ app_admin/src/app/data/trips.ts
@@ -0,0 +1,32 @@
+[
+    {
+      "code": "GALR210214",
+      "name": "Gale Reef",
+      "length": "6 nights, 7 days",
+      "start": "2021-02-14T08:00:00Z",
+      "resort": "Sapphire Bay, 4 stars",
+      "perPerson": "499.00",
+      "image": "reef1.jpg",
+      "description": "<p>at Gale Reef..... sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis."
+    },
+    {
+      "code": "DWAR210315",
+      "name": "Dawson's Dwarf Reef",
+      "length": "4 nights / 5 days",
+      "start": "2021-03-15T08:00:00Z",
+      "resort": "The Mining Guild, 4 stars",
+      "perPerson": "1199.00",
+      "image": "reef2.jpg",
+      "description": "<p>at Dawson's Dwarf Reef.... integer magna leo, posuere et dignissim vitae, porttitor at odio. Pellentesque a metus nec magna placerat volutpat. Nunc nisi mi, elementum sit amet aliquet quis, tristique quis nisl. Curabitur odio lacus, blandit ut hendrerit."
+    },
+    {
+      "code": "CLAR210621",
+      "name": "Falador Park",
+      "length": "4 nights / 5 days",
+      "start": "2021-06-21T08:00:00Z",
+      "resort": "Coral Bay Sands, 5 stars",
+      "perPerson": "3999.00",
+      "image": "reef3.jpg",
+      "description": "<p>At Falador Park..... donec sed felis risus. Nulla facilisi. Donec a orci tellus, et auctor odio. Fusce ac orci nibh, quis semper arcu. Cras orci neque, euismod et accumsan ac, sagittis molestie lorem. Proin odio sapien, elementum at tempor non."
+    }
+  ]

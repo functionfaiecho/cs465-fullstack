d9e03a4fda3d3414f245e15ce0e2c7fe8c1313fb
functionfaiecho <nurfaizahmkhalik@outlook.com>
2024-06-20 22:09:04 +1200

    From page 50 onwards let's go

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -2,17 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Trip} from '../models/trip';
+import { Trip } from '../models/trip';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TripDataService {
+  constructor(private http: HttpClient) {}
+  url = 'http://localhost:3000/api/trips';
 
-  constructor(private http: HttpClient) { }
+  getTrips(): Observable<Trip[]> {
+    return this.http.get<Trip[]>(this.url);
+  }
 
-  getTrips() : Observable<Trip[]> {
-    let url = 'http://localhost:3000/api/trips';
+  addTrip(formData: Trip): Observable<Trip[]> {
+    return this.http.post<Trip[]>(this.url, formData);
+  }
 
-    return this.http.get<Trip[]>(url);
+  getTrip(tripCode: string): Observable<Trip[]> {
+    return this.http.get<Trip[]>(this.url + '/' + tripCode);
+  }
+
+  updateTrip(formData: Trip): Observable<Trip[]> {
+    return this.http.put<Trip[]>(this.url + '/' + formData.code, formData);
   }
 }
